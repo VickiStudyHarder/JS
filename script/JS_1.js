afa519fae715872faca99ad802766aa90eed7fc5
VickiStudyHarder <vickichen98@gmail.com>
2023-07-17 16:22:32 +1000

    codemirror.

--- script/JS_1.js
+++ script/JS_1.js
@@ -99,10 +99,317 @@ console.log(b);
 //otherwise: good evening;
 let hour = 10;
 
-if (condition) {
-  statement;
-} else if (condition) {
-  statement;
+if (hour >= 6 && hour < 12) {
+  console.log("Good morning");
+} else if (hour >= 12 && hour < 18) {
+  console.log("Good afternoon");
 } else {
-  statement;
+  console.log("Good evening");
 }
+
+//switch...case
+let role;
+
+switch (role) {
+  case "guest":
+    console.log("Guest User");
+    break; //if not break it will execute to next block
+
+  case "moderator":
+    console.log("Moderator User");
+    break;
+
+  default:
+    console.log("Unknown User");
+  //no need to break
+}
+
+//for loop
+for (let i = 0; i < 5; i++) {
+  console.log(i);
+}
+
+console.log("odd number");
+for (let i = 1; i < 5; i++) {
+  if (i % 2 != 0) console.log(i);
+}
+
+console.log("odd number reverse");
+for (let i = 5; i > 0; i--) {
+  if (i % 2 != 0) console.log(i);
+}
+
+//while loop
+console.log("while loop");
+let i = 0;
+while (i <= 5) {
+  if (i % 2 != 0) console.log(i);
+  i++;
+}
+// do while loop
+console.log("do while loop");
+let z = 9;
+do {
+  if (z % 2 != 0) console.log(z);
+  z++;
+} while (z <= 5);
+
+//for in loop
+const person1 = {
+  name: "vicki",
+  age: 30,
+};
+
+for (let key in person1) console.log(key, person1[key]);
+
+const colors = ["red", "blue", "green"];
+for (let index in colors) console.log(index, colors[index]);
+
+//for of loop
+for (let color of colors) {
+  console.log(color);
+}
+
+//break and continue
+let q = 0;
+while (q <= 10) {
+  if (q % 2 === 0) {
+    q++;
+    continue;
+  }
+  console.log(q);
+  q++;
+}
+//exercise
+//exercise 1
+function maxNumber(num1, num2) {
+  //   if (num1 > num2) {
+  //     return num1;
+  //   } else {
+  //     return num2;
+  //   }
+  return num1 > num2 ? num1 : num2;
+}
+console.log("maxNumber" + maxNumber(12, 11));
+
+//exercise2
+function isLandscape(width, height) {
+  return width > height;
+  //no need to add true or false as evaluating the condition will return true or false
+}
+
+//exercise3
+//divisible by 3 => fizz
+//divisible by 5 => buzz
+//divisible by 3 and 5 => fizzbuzz
+//not divisible by 3 or 5 => return number
+//input not a number => 'not a number'
+
+function fizzBuzz(num) {
+  if (typeof num !== "number") return NaN;
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  }
+  if (num % 3 === 0) {
+    return "Fizz";
+  }
+  if (num % 5 === 0) {
+    return "Buzz";
+  }
+  return num;
+}
+
+console.log(fizzBuzz(""));
+
+//exercise 4
+//speed limit = 70
+// 5 -> 1 point
+//more then 12 point -> suspended
+
+function checkSpeed(speed) {
+  const speedLimit = 70;
+  const kmPerPoint = 5;
+
+  if (speed < speedLimit + kmPerPoint) {
+    return "ok";
+  }
+  let point = Math.floor((speed - speedLimit) / 5);
+  if (point < 12) return point + " point";
+  else {
+    return "license suspended";
+  }
+}
+
+console.log(checkSpeed(80));
+
+//exercise 5
+function showNumber(limit) {
+  for (let c = 0; c <= limit; c++) {
+    const message = c % 2 === 0 ? "EVEN" : "ODD";
+    console.log(c, message);
+    // if (c % 2 == 0) console.log(c, " EVEN");
+    // else {
+    //   console.log(c, " ODD");
+    // }
+  }
+}
+showNumber(10);
+
+//exercise 6
+array = [undefined, null, 0, "", false, NaN, 2];
+function countTruthy(array) {
+  let count = 0;
+  for (let value of array) if (value) count++;
+  return count;
+}
+console.log(countTruthy(array));
+
+//exercise 7
+//we want to display string type properties only
+const movie = {
+  title: "a",
+  releaseYear: 2018,
+  rating: 4.5,
+  director: "vicki",
+};
+
+function showProperties(obj) {
+  for (key in obj) if (typeof obj[key] === "string") console.log(key, obj[key]);
+}
+showProperties(movie);
+
+//exercise 8
+//return all the multiple of 3: 3 6 9
+//return all the multiple of 5: 5 10 15
+//return the sum of all these multiple
+
+function sum(limit) {
+  let result = 0;
+  for (let i = 0; i <= limit; i++) {
+    if (i % 3 == 0 || i % 5 == 0) result += i;
+    //or will get 3, 5, 3 and 5
+  }
+  console.log(result);
+}
+
+sum(15);
+
+//exercise 9
+mark = [70, 80, 40];
+function calculateAverage(array) {
+  let sum = 0;
+  for (let value in array) sum += value;
+  return sum / array.length;
+}
+function calculateGrade(array) {
+  average = calculateAverage(array);
+  if (average < 60) return "F"; //function end with return
+  if (average < 70) return "D";
+  if (average < 80) return "C";
+  if (average < 90) return "B";
+  return A;
+}
+
+console.log("mark", calculateGrade(mark));
+
+//exercise 10
+function showStar(rows) {
+  for (let i = 1; i <= rows; i++) {
+    let pattern = "";
+    for (let j = 1; j <= i; j++) pattern += "*";
+    console.log(pattern);
+  }
+}
+showStar(5);
+
+//exercise 11
+//prime number is a number that can only evenly divide by it self and 1
+function isPrime(number) {
+  for (let factor = 2; factor < number; factor++)
+    if (number % factor === 0) return false;
+  return true;
+}
+
+function showPrime(limit) {
+  for (let number = 2; number <= limit; number++)
+    // let isPrime = true;
+    // for (let factor = 2; factor < number; factor++)
+    //   if (number % factor === 0) {
+    //     isPrime = false;
+    //     break; //if it can be divide then no need to test the rest of the factor
+    //   }
+
+    if (isPrime(number)) console.log(number);
+}
+//we can transfer this to two functions
+
+showPrime(10);
+
+//object
+//basic
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  draw: function () {
+    console.log("draw");
+  },
+};
+circle.draw();
+
+//factory function
+// function createCircle(radius, location) {
+//     //radius and draw are method name
+//   return {
+//     radius: radius,
+//     draw: function () {
+//         console.log("draw");
+//       }
+//   };
+// }
+
+//same function to above, shorter syntax
+function createCircle(radius, location) {
+  //radius and draw are method name
+  return {
+    radius,
+    draw() {
+      console.log("draw");
+    },
+  };
+}
+
+const circle1 = createCircle(10);
+// console.log("circle1: ", circle1);
+// console.log("circle1: ",circle1.draw)
+circle1.draw();
+
+//constructor function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function() {
+        console.log("draw");
+    }
+}
+//use the new keyword, we create an empty object 
+//then it will set this to point to that object
+// then set circle2 point to this object
+const circle2 = new Circle(1);
+
+// value vs reference type
+let x1 = 10; 
+let y1 = x1; 
+x1 = 20;
+console.log('x1: ', x1)
+console.log('y1: ', y1)
+
+let x2 = {value: 10}; 
+let y2 = x2; 
+x2.value = 20;
+console.log('x2: ', x2)
+console.log('y2: ', y2)
+
+// enumerating properties of an object
+for (let key of Object.keys(circle1)) console.log(key)
--- script/codemirror.js
+++ script/codemirror.js
@@ -1,38 +1,100 @@
-var codeEditor = CodeMirror.fromTextArea(
-  document.getElementById("code-editor"),
-  {
-    mode: "javascript",
-    lineNumbers: true,
-    theme: "eclipse"
+// var codeEditor = CodeMirror.fromTextArea(
+//   document.getElementById("code-editor"),
+//   {
+//     mode: "javascript",
+//     lineNumbers: true,
+//     theme: "eclipse",
+//     autoRefresh: true
+//   }
+// );
+// codeEditor.setSize('100%','80%')
+
+
+// var consoleElement = document.getElementById("console");
+// var runButton = document.getElementById("run-button");
+
+// function redirectConsoleLog() {
+//   var originalLog = console.log;
+//   console.log = function (message) {
+//     var outputLine = document.createElement("div");
+//     outputLine.textContent = message;
+//     consoleElement.appendChild(outputLine);
+//     originalLog.apply(console, arguments);
+//   };
+// }
+
+// function runCode() {
+//   // 执行代码
+//   try {
+//     eval(codeEditor.getValue());
+//   } catch (error) {
+//     console.log("Error: " + error.message);
+//   }
+// }
+
+// runButton.addEventListener("click", runCode);
+
+// // 页面加载完成后执行的逻辑
+// window.addEventListener("load", function () {
+//   redirectConsoleLog();
+//   runCode();
+// });
+
+function createCodeEditor(elementId, options) {
+  var codeEditor = CodeMirror.fromTextArea(document.getElementById(elementId), options);
+
+  function redirectConsoleLog(consoleElement) {
+    var originalLog = console.log;
+    console.log = function (message) {
+      var outputLine = document.createElement("div");
+      outputLine.textContent = message;
+      consoleElement.appendChild(outputLine);
+      originalLog.apply(console, arguments);
+    };
   }
-);
 
-var consoleElement = document.getElementById("console");
-var runButton = document.getElementById("run-button");
+  function runCode(codeEditor, consoleElement) {
+    // 执行代码
+    try {
+      eval(codeEditor.getValue());
+    } catch (error) {
+      console.log("Error: " + error.message);
+    }
+  }
 
-function redirectConsoleLog() {
-  var originalLog = console.log;
-  console.log = function (message) {
-    var outputLine = document.createElement("div");
-    outputLine.textContent = message;
-    consoleElement.appendChild(outputLine);
-    originalLog.apply(console, arguments);
-  };
+  var consoleElement = document.getElementById(options.consoleId);
+  var runButton = document.getElementById(options.runButtonId);
+
+  runButton.addEventListener("click", function () {
+    runCode(codeEditor, consoleElement);
+  });
+
+  // 页面加载完成后执行的逻辑
+  window.addEventListener("load", function () {
+    redirectConsoleLog(consoleElement);
+    runCode(codeEditor, consoleElement);
+  });
+
+  return codeEditor;
 }
 
-function runCode() {
-  // 执行代码
-  try {
-    eval(codeEditor.getValue());
-  } catch (error) {
-    console.log("Error: " + error.message);
-  }
-}
-
-runButton.addEventListener("click", runCode);
-
-// 页面加载完成后执行的逻辑
-window.addEventListener("load", function () {
-  redirectConsoleLog();
-  runCode();
+// 创建第一个编辑器
+var codeEditor1 = createCodeEditor("code-editor1", {
+  mode: "javascript",
+  lineNumbers: true,
+  theme: "eclipse",
+  autoRefresh: true,
+  consoleId: "console1",
+  runButtonId: "run-button1"
 });
+
+// 创建第二个编辑器
+var codeEditor2 = createCodeEditor("code-editor2", {
+  mode: "htmlmixed",
+  lineNumbers: true,
+  theme: "material",
+  autoRefresh: true,
+  consoleId: "console2",
+  runButtonId: "run-button2"
+});
+codeEditor2.setSize('100%','90%')
